07a85b42ac6e16511000df307f44fb5ad4b5969d
unknown <agusmendez753@gmail.com>
2021-01-31 16:48:50 -0300

    Más Cambios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,19 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+//El dato prompt, a diferencia de "alert", siene dos botones: el de aceptar y cancelar.
+// El dato prompt permite ingresar un dato 
 function mostrar()
 {
-	alert("ok");
+
+	let nombre;
+
+	nombre = prompt("Introduzca su nombre")
+
+	/*Solo se puede guardar un dato en la variable. Si yo inicializo  una variable con un nombre y 
+	luego le doy otro nombre, el primer dato se "pisa" y se toma el segundo nombre */
+
+	alert(nombre);
 }
 
